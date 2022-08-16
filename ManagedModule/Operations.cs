dc1fabeca27b04b085e7935589f4fcc59e014989
Faik Hakan Bilgen <faikb@microsoft.com>
2022-08-16 18:48:40 +0300

    Added the Operations class

--- ManagedModule/Operations.cs
+++ ManagedModule/Operations.cs
@@ -2,6 +2,13 @@
 {
     public class Operations
     {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public int Multiply(int a, int b) => a * b;
+
+        public int Divide(int a, int b) => a / b;
 
     }
 }
